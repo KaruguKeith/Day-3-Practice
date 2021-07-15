3ee88a2462cefe8655e3b0f02ce8803ea6435d70
KaruguKeith <karugukeith@gmail.com>
2021-07-15 17:23:04 +0300

    For loops

---
+++ students.py
@@ -0,0 +1,12 @@
+
+students = [{"name" : "keith" , "age" : 20, "Gender" : "male" },
+                {"name" : "john" , "age" : 19, "Gender" : "male" },
+                {"name" : "magret" , "age" : 27, "Gender" : "female" },
+                {"name" : "mary" , "age" : 20, "Gender" : "female" },
+                {"name" : "janet" , "age" : 21, "Gender" : "female" },
+                {"name" : "paul" , "age" : 20, "Gender" : "male" }]
+
+for student in students:
+        print(student['name'] ,
+            student['age'] ,
+            student['Gender'])
